27665ea56bd87c6bb032955d6f0274af77a6700a
Lucas Duarte Bareli <42220020894@>
2025-05-16 14:01:30 -0300

    16-05-2025

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -16,32 +16,29 @@ const Footer = () => {
                     <h3 className="text-[96px] font-semibold league-regular text-black leading-none">
                         SMART <span className="text-[#17CF96] font-semibold league-regular">CITY</span>
                     </h3>
-                </div>
 
-                {/* Coluna 3: Contatos */}
-                <div className="text-center">
                     <p className="text-black font-thin league-regular text-[24px]">Contacts</p>
                     <p className="text-black font-thin league-regular text-[24px]">+55 19 9999-9999</p>
                     <p className="text-black font-thin league-regular text-[24px]">lucasnareli@gmail.com</p>
                 </div>
 
             {/* Linha 4: Links de navegação */}
-            <div className="!mt-10  !mb-10 flex justify-between items-center flex-col md:flex-row gap-4">
+            <div className="!mt-10 !mb-10 !ml-15 flex justify-between items-center flex-col md:flex-row gap-4">
                 {/* Navegação */}
                 <nav className="flex gap-6">
-                    <a href="#home" className="text-black text-[20px] font-semibold league-regular hover:underline">
+                    <a href="/home" className="text-black text-[20px] font-semibold league-regular hover:underline">
                         Home
                     </a>
-                    <a href="#sensors" className="text-black text-[20px] font-semibold league-regular hover:underline">
+                    <a href="/sensors" className="text-black text-[20px] font-semibold league-regular hover:underline">
                         Sensors
                     </a>
-                    <a href="#data" className="text-black text-[20px] font-semibold league-regular hover:underline">
+                    <a href="/data" className="text-black text-[20px] font-semibold league-regular hover:underline">
                         Data
                     </a>
                 </nav>
 
                 {/* Ícones sociais */}
-                <div className="flex gap-4">
+                <div className="flex gap-4 !mr-15">
                     <FaLinkedin className="w-8 h-8 text-black cursor-pointer transition duration-300 hover:text-[#17CF96]" />
                     <FaGithub className="w-8 h-8 text-black cursor-pointer transition duration-300 hover:text-[#17CF96]" />
                     <FaInstagram className="w-8 h-8 text-black cursor-pointer transition duration-300 hover:text-[#17CF96]" />
